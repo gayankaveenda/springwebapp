dae5520f60651c570c84329cd6c27e73f46135af
Gayan Kaveenda <gayankaveenda@gmail.com>
2023-05-04 21:38:17 +1000

    Initial Project Setup

---
+++ src/main/java/com/information/learn/springwebapp/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.information.learn.springwebapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.information.learn.springwebapp")
+public class WebConfig {
+}
